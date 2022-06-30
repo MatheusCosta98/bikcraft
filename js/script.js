4bf2514cafc3b65ae7b4242d002363099a3e6384
MatheusCosta98 <matheus.98cs@gmail.com>
2022-06-30 01:00:47 -0300

    otimizado

---
+++ js/script.js
@@ -0,0 +1,81 @@
+
+// ativar links do menu 
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  
+  if (url.includes (href)) {
+    link.classList.add ("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// ativar itens do orçamento 
+
+const parametros = new URLSearchParams(location.search) ; 
+
+function ativarProduto (parametros){
+  const elemento = document.getElementById(parametros);
+  if (elemento) { 
+  elemento.checked = true;
+}
+ 
+}
+
+parametros.forEach (ativarProduto);
+
+ 
+
+// perguntas
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  
+  const ativa = resposta.classList.contains('ativa');
+  
+  pergunta.setAttribute("aria-expanded", ativa);
+
+}
+
+function eventosPerguntas (pergunta) {
+ pergunta.addEventListener('click', ativarPergunta);
+}
+ perguntas.forEach (eventosPerguntas); 
+
+ // galeria de bicivletas 
+
+ const galeria = document.querySelectorAll (".bicicleta-imagem img");
+ const galeriaContainer = document.querySelector('.bicicleta-imagem');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media =  matchMedia('(min-width: 1000px') .matches;
+  console.log (media);
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+  
+  
+}
+
+function eventosGaleria (img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+ galeria.forEach (eventosGaleria);
+
+ // animaçao
+if (window.SimpleAnime) {
+  new SimpleAnime(); 
+}
+ 
